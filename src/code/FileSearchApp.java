7a4b0778674e6d6e7e1aee64d460f32516b351c4
Orlando Rodriguez <orlando.r@me.com>
2019-02-01 21:54:50 -0700

    created main method with limit of 3 arguments

--- src/code/FileSearchApp.java
+++ src/code/FileSearchApp.java
@@ -1,12 +1,57 @@
 package code;
 
 public class FileSearchApp {
-
+	String path;
+	String regex;
+	String zipFileName;
+	
+	
 	public static void main(String[] args) {
-		for (String arg : args) {
-			System.out.println(arg);
+		FileSearchApp app = new FileSearchApp();
+		
+		switch(Math.min(args.length, 3)) {
+		case 0:
+			System.out.println("USAGE: FileSearchapp path [regex] [zipfile]");
+			return;
+		case 3: app.setZipFileName(args[2]);
+		case 2: app.setRegex(args[1]);
+		case 1: app.setPath(args[0]);
 		}
+		try {
+			app.walkDirectory(app.getPath());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
+
+	public String getPath() {
+		return path;
+	}
+
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+
+	public String getRegex() {
+		return regex;
+	}
+
+
+	public void setRegex(String regex) {
+		this.regex = regex;
+	}
+
+
+	public String getZipFileName() {
+		return zipFileName;
+	}
+
+
+	public void setZipFileName(String zipFileName) {
+		this.zipFileName = zipFileName;
 	}
 
 }
